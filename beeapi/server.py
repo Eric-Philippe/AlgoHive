910c65730753da6592103a2aa5f89737ccdfce2f
Eric-Philippe <ericphlpp@gmail.com>
2025-03-08 19:40:59 +0100

    Switch Docker base image to Alpine for a smaller footprint and add signal handling for graceful shutdown

--- beeapi/server.py
+++ beeapi/server.py
@@ -1,14 +1,23 @@
+import sys
+import signal
 import atexit
 from app import app, loader
 
 def on_exit():
     loader.unload()
+    
+def handle_signal(signum, frame):
+    on_exit()
+    sys.exit(0)
 
 if __name__ == '__main__':
     loader.extract()
     loader.load()
     
     atexit.register(on_exit)
+    
+    signal.signal(signal.SIGTERM, handle_signal)
+    signal.signal(signal.SIGINT, handle_signal)
     
     app.run(host='0.0.0.0')
     
